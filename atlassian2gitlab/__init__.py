848a08c595d71e034951495a7f7e364c7aff2de3
Yoann Colin <ycolin@oxeva.fr>
2018-01-18 17:02:37 +0100

    Flush the Gitlab project
    Issue #4

--- atlassian2gitlab/__init__.py
+++ atlassian2gitlab/__init__.py
@@ -16,8 +16,9 @@ ssl_verify = True
 
 
 def issues():
+    logger = logging.getLogger(__name__)
     if not ssl_verify:
-        logging.warn('You disabled SSL checks, it is not recommanded !')
+        logger.warn('You disabled SSL checks, it is not recommanded !')
 
     gitlab = Gitlab(gitlab_url, private_token=gitlab_token,
                     api_version=4, ssl_verify=ssl_verify)
@@ -28,8 +29,39 @@ def issues():
         gitlab.enable_debug()
 
     project = gitlab.search_project_from_repo(gitlab_repo)
+    issues = jira.active_issues(jira_project_key)
 
-    for issue in jira.active_issues(jira_project_key):
-        print(project.issues.create({
-            'title': issue.fields.summary
-        }))
+    logger.info('{} issues to migrate'.format(len(issues)))
+    i = 0
+    for jira_issue in issues:
+        gl_issue = project.issues.create({
+            'title': jira_issue.fields.summary
+        })
+        logger.debug('Issue {} migrated: #{}'.format(jira_issue, gl_issue.id))
+        i += 1
+
+    logger.info('{} issues migrated'.format(i))
+
+
+def flush():
+    logger = logging.getLogger(__name__)
+    if not ssl_verify:
+        logger.warn('You disabled SSL checks, it is not recommanded !')
+
+    gitlab = Gitlab(gitlab_url, private_token=gitlab_token,
+                    api_version=4, ssl_verify=ssl_verify)
+
+    if debug:
+        gitlab.enable_debug()
+
+    project = gitlab.search_project_from_repo(gitlab_repo)
+    issues = project.issues.list(all=True)
+
+    logger.info('{} issues to delete'.format(len(issues)))
+    i = 0
+    for issue in issues:
+        issue.delete()
+        logger.debug('Issue {} deleted'.format(issue.id))
+        i += 1
+
+    logger.info('{} issues deleted'.format(i))
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,64 +1,68 @@
 import sys
 from atlassian2gitlab.cli import CLI
 
-EXPECTED_ARGS = ['atlassian2gitlab',
-                 '--gitlab-token', 'foo',
-                 '--gitlab-repo', 'bar',
-                 '--atlassian-username', 'john@doe.tld',
-                 '--atlassian-password', 'secret',
-                 '--jira-project-key', 'KEY']
+FLUSH_ARGS = ['flush-gitlab',
+              '--gitlab-token', 'foo',
+              '--gitlab-repo', 'bar']
+
+MIGRATION_ARGS = ['atlassian2gitlab',
+                  '--gitlab-token', 'foo',
+                  '--gitlab-repo', 'bar',
+                  '--atlassian-username', 'john@doe.tld',
+                  '--atlassian-password', 'secret',
+                  '--jira-project-key', 'KEY']
 
 
 def test_gl_url_default():
-    sys.argv = EXPECTED_ARGS
-    assert CLI().config.GL_URL == 'https://gitlab.com/'
+    sys.argv = FLUSH_ARGS
+    assert CLI().flushConfig.GL_URL == 'https://gitlab.com/'
 
 
 def test_gl_url():
-    sys.argv = EXPECTED_ARGS
+    sys.argv = FLUSH_ARGS
     sys.argv.extend(['--gitlab-url', 'https://test.tld'])
-    assert CLI().config.GL_URL == 'https://test.tld'
+    assert CLI().flushConfig.GL_URL == 'https://test.tld'
 
 
 def test_gl_token():
-    sys.argv = EXPECTED_ARGS
-    assert CLI().config.GL_TOKEN == 'foo'
+    sys.argv = FLUSH_ARGS
+    assert CLI().flushConfig.GL_TOKEN == 'foo'
 
 
 def test_gl_repo():
-    sys.argv = EXPECTED_ARGS
-    assert CLI().config.GL_REPO == 'bar'
+    sys.argv = FLUSH_ARGS
+    assert CLI().flushConfig.GL_REPO == 'bar'
 
 
 def test_at_user():
-    sys.argv = EXPECTED_ARGS
-    assert CLI().config.AT_USER == 'john@doe.tld'
+    sys.argv = MIGRATION_ARGS
+    assert CLI().migrationConfig.AT_USER == 'john@doe.tld'
 
 
 def test_at_pass():
-    sys.argv = EXPECTED_ARGS
-    assert CLI().config.AT_PASS == 'secret'
+    sys.argv = MIGRATION_ARGS
+    assert CLI().migrationConfig.AT_PASS == 'secret'
 
 
 def test_jira_url_default():
-    sys.argv = EXPECTED_ARGS
-    assert CLI().config.JIRA_URL == 'https://jira.atlassian.com'
+    sys.argv = MIGRATION_ARGS
+    assert CLI().migrationConfig.JIRA_URL == 'https://jira.atlassian.com'
 
 
 def test_jira_url():
-    sys.argv = EXPECTED_ARGS
+    sys.argv = MIGRATION_ARGS
     sys.argv.extend(['--jira-url', 'https://test.tld'])
-    assert CLI().config.JIRA_URL == 'https://test.tld'
+    assert CLI().migrationConfig.JIRA_URL == 'https://test.tld'
 
 
 def test_jira_project_key():
-    sys.argv = EXPECTED_ARGS
-    assert CLI().config.JIRA_PROJECT_KEY == 'KEY'
+    sys.argv = MIGRATION_ARGS
+    assert CLI().migrationConfig.JIRA_PROJECT_KEY == 'KEY'
 
 
 def test_warn_is_yellow(capsys):
     import logging
-    cli = CLI()
+    CLI()
     logging.warn('message')
     out, err = capsys.readouterr()
-    assert err == '\x1b[33mWARNING message\x1b[0m\n'
+    assert err == '\x1b[33mWARNING  root           message\x1b[0m\n'
