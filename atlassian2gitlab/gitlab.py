e8ce429c2cc32125f6704c1f6bdb14faa3713a76
Yoann Colin <ycolin@oxeva.fr>
2018-01-23 17:17:48 +0100

    Add the assignee
    Issue #6

--- atlassian2gitlab/gitlab.py
+++ atlassian2gitlab/gitlab.py
@@ -1,8 +1,10 @@
 import gitlab
+import logging
 
 
 class Gitlab(gitlab.Gitlab):
     """Extension of python-gilatb for our usage"""
+    _users = {}
 
     def search_project_from_repo(self, repo):
         """
@@ -20,3 +22,40 @@ class Gitlab(gitlab.Gitlab):
             if project.path_with_namespace == repo:
                 return project
         raise Exception("Project {} not found".format(repo))
+
+    def get_user_by_name(self, name):
+        """
+        Find the user by his name
+
+        Raises:
+            Exception: Not found
+
+        Returns:
+            gitlab.v4.objects.User
+        """
+        if name in self._users:
+            return self._users[name]
+
+        users = self.users.list(username=name)
+        if len(users) == 1:
+            self._users[name] = users[0]
+            return users[0]
+
+        raise Exception('{} users matching {} found'.format(len(users), name))
+
+    def create_from_jira(self, project, issue):
+        """
+        Create an issue from a Jira issue object
+
+        Returns:
+            gitlab.v4.objects.Issue
+        """
+        data = {
+            'title': issue.fields.summary
+        }
+
+        if issue.fields.assignee:
+            assignee = self.get_user_by_name(issue.fields.assignee.name)
+            data['assignee_ids'] = [assignee.id]
+
+        return project.issues.create(data)
